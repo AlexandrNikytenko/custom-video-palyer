52d71ee3df51b649d388d620c828c6a004c35b08
AlexandrNikytenko <aleksander.nikytenko.cubex@gmail.com>
2024-09-06 17:09:18 +0300

    first commit

--- README.md
+++ README.md
@@ -1,70 +1,17 @@
-# Getting Started with Create React App
+## Custom HTML5 Video Player with Controls
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project is a custom video player built using React that enhances the native HTML5 video element by providing a custom control interface. The main goal of this project is to demonstrate how to implement custom controls for HTML5 video, replacing the native browser controls with a more personalized user interface.
 
-## Available Scripts
+### Features:
 
-In the project directory, you can run:
+- **Play/Pause**: The user can toggle between playing and pausing the video by clicking either on the video itself or a custom Play/Pause button.
+- **Seek Bar**: A progress bar allows the user to navigate through the video by dragging or clicking along the bar.
+- **Volume Control**: A slider enables the user to adjust the video volume to their preference.
+- **Fullscreen Mode**: The user can toggle fullscreen mode using a dedicated button.
+- **Skip Forward/Backward**: The user can quickly skip forward or backward in the video by 10 seconds using dedicated skip buttons.
+- **Playback Speed Control**: The user can modify the video playback speed, with options ranging from 0.5x to 2x.
+- **Time Display**: The current playback time and total video duration are displayed in a readable format (mm:ss).
 
-### `npm start`
+### Purpose:
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+This project serves as an example of how to integrate custom control elements with the native HTML5 video player, providing users with a more customizable and intuitive video playback experience. The default browser video controls are replaced with this custom implementation to offer a personalized user interface.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,182 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef, useEffect } from "react";
+import video from "./video/217262_small.mp4";
 
 function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [volume, setVolume] = useState(1);
+  const [playbackRate, setPlaybackRate] = useState(1); // Начальная скорость воспроизведения
+  const videoRef = useRef(null);
+  const progressRef = useRef(null);
+  const volumeRef = useRef(null);
+  const appRef = useRef(null);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+  };
+
+  const togglePlay = () => {
+    if (videoRef.current.paused) {
+      videoRef.current.play();
+      setIsPlaying(true);
+    } else {
+      videoRef.current.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  const handleProgress = () => {
+    const value =
+      (videoRef.current.currentTime / videoRef.current.duration) * 100;
+    progressRef.current.value = value;
+    setCurrentTime(videoRef.current.currentTime);
+  };
+
+  const handleSeek = (e) => {
+    const seekTime = (e.target.value / 100) * videoRef.current.duration;
+    videoRef.current.currentTime = seekTime;
+    setCurrentTime(seekTime);
+  };
+
+  const handleVolumeChange = (e) => {
+    const vol = e.target.value;
+    videoRef.current.volume = vol;
+    setVolume(vol);
+  };
+
+  const handleVolumeToggle = (e) => {
+    if (videoRef.current.volume === 0) {
+      videoRef.current.volume = 1;
+      setVolume(1);
+    } else {
+      videoRef.current.volume = 0;
+      setVolume(0);
+    }
+  };
+
+  const toggleFullscreen = () => {
+    if (document.fullscreenElement) document.exitFullscreen();
+    else appRef.current.requestFullscreen();
+  };
+
+  const skipForward = () => {
+    videoRef.current.currentTime = Math.min(
+      videoRef.current.currentTime + 10,
+      videoRef.current.duration
+    );
+    setCurrentTime(videoRef.current.currentTime);
+  };
+
+  const skipBackward = () => {
+    videoRef.current.currentTime = Math.max(
+      videoRef.current.currentTime - 10,
+      0
+    );
+    setCurrentTime(videoRef.current.currentTime);
+  };
+
+  const changePlaybackRate = (rate) => {
+    videoRef.current.playbackRate = rate;
+    setPlaybackRate(rate);
+  };
+
+  useEffect(() => {
+    const video = videoRef.current;
+
+    video.onloadedmetadata = () => {
+      setDuration(video.duration);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" ref={appRef}>
+      <div className="video-container">
+        <video
+          ref={videoRef}
+          className="custom-video"
+          onClick={togglePlay}
+          onTimeUpdate={handleProgress}
+          volume={volume}
         >
-          Learn React
-        </a>
-      </header>
+          <source src={video} type="video/mp4" />
+          Ваш браузер не поддерживает видео тег.
+        </video>
+      </div>
+      <div className="controls">
+        <div className="options">
+          <button onClick={togglePlay}>{isPlaying ? "Pause" : "Play"}</button>
+          <input
+            ref={progressRef}
+            type="range"
+            className="progress"
+            min="0"
+            max="100"
+            step="0.1"
+            defaultValue="0"
+            onChange={handleSeek}
+          />
+
+          <button onClick={skipBackward}>⏪ 10 sec</button>
+          <button onClick={skipForward}>10 sec ⏩</button>
+          <button onClick={toggleFullscreen}>Fullscreen</button>
+        </div>
+
+        <div className="playback-rate-controls">
+          <span>Speed: </span>
+          <button
+            onClick={() => changePlaybackRate(0.5)}
+            disabled={playbackRate === 0.5}
+            style={playbackRate === 0.5 ? {opacity: '0.5'} : {}}
+          >
+            0.5x
+          </button>
+          <button
+            onClick={() => changePlaybackRate(1)}
+            disabled={playbackRate === 1}
+            style={playbackRate === 1 ? {opacity: '0.5'} : {}}
+          >
+            1x (Normal)
+          </button>
+          <button
+            onClick={() => changePlaybackRate(1.5)}
+            disabled={playbackRate === 1.5}
+            style={playbackRate === 1.5 ? {opacity: '0.5'} : {}}
+          >
+            1.5x
+          </button>
+          <button
+            onClick={() => changePlaybackRate(2)}
+            disabled={playbackRate === 2}
+            style={playbackRate === 2 ? {opacity: '0.5'} : {}}
+          >
+            2x
+          </button>
+        </div>
+
+        <div className="playback-rate-controls">
+          Time: {formatTime(currentTime)} / {formatTime(duration)}
+        </div>
+        <div className="playback-rate-controls">
+          Volume:
+          <input
+            ref={volumeRef}
+            type="range"
+            className="volume"
+            min="0"
+            max="1"
+            step="0.01"
+            value={volume}
+            onChange={handleVolumeChange}
+          />
+          <button onClick={handleVolumeToggle}>
+            Volume {volume === 1 ? "Off" : "On"}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
